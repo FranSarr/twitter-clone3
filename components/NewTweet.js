9a48dd45ccded9d5b679de54924a8832458c555f
fsarrias <fsarrias@kolab.es>
2022-06-03 16:16:36 +0200

    I create the tweet APi end point

--- components/NewTweet.js
+++ components/NewTweet.js
@@ -17,7 +17,15 @@ export default function NewTweet() {
           return
         }
 
-        alert(content)
+        fetch('/api/tweet', {
+            body: JSON.stringify({
+              content,
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+          })
       }}
     >
       <div className='flex'>
