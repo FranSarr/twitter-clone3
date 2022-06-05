fc2b2ff8504b0bfff7c0f589258dda5972ce1cba
fsarrias <fsarrias@kolab.es>
2022-06-05 14:11:17 +0200

    I implement the delete tweet

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import Tweets from 'components/Tweets'
+import prisma from 'lib/prisma'
+import { getTweets } from 'lib/data.js'
 
-export default function Home() {
+export default function Welcome({tweets}) {
 
   const { data: session, status } = useSession()
   const router = useRouter()
@@ -14,5 +17,29 @@ export default function Home() {
     router.push('/home')
   }
 
-  return <a href='/api/auth/signin'>login</a>
+  return (
+		<div className='mt-10'>
+       <Tweets tweets={tweets} />
+      <p className='text-center p-4 border m-4'>
+        <h2 className='mb-10'>Join the conversation!</h2>
+        <a
+          className='border px-8 py-2 mt-5 font-bold rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker'
+          href='/api/auth/signin'
+        >
+          login
+        </a>
+      </p>
+    </div>  
+  )
+}
+export async function getServerSideProps() {
+	const take = 3
+  let tweets = await getTweets(prisma, take)
+  tweets = JSON.parse(JSON.stringify(tweets))
+
+  return {
+    props: {
+      tweets,
+    },
+  }
 }
